7ec845746e7f897cf8a4b97762b8f2d9e10b6504
ParsaBordbar <parsab71@gmail.com>
2025-01-26 13:58:10 +0330

    feat: extended_fingers func to detect extended fingers

--- hand_landmarks.py
+++ hand_landmarks.py
@@ -31,4 +31,12 @@ def get_finger_data(hand_landmarks):
             "pinky_tip": hand_landmarks.landmark[20],
         }
     }
-    
+
+def get_extended_fingers(fingers):
+    return {
+        'thumb': fingers['thumb']['thumb_tip'].y < fingers['thumb']['thumb_ip'].y,
+        'index': fingers['index']['index_tip'].y < fingers['index']['index_pip'].y,
+        'middle': fingers['middle']['middle_tip'].y < fingers['middle']['middle_pip'].y,
+        'ring': fingers['ring']['ring_tip'].y < fingers['ring']['ring_pip'].y,
+        'pinky': fingers['pinky']['pinky_tip'].y < fingers['pinky']['pinky_pip'].y
+    }
