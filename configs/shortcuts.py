5466bf87d369a3be7ac752db60e89136f5f23bc5
ParsaBordbar <parsab71@gmail.com>
2025-01-26 18:55:30 +0330

    feat: config dir, shortcuts file

---
+++ configs/shortcuts.py
@@ -0,0 +1,33 @@
+# Note: more dynamic support for shortcuts and maps for future gestures are on way stay toned!
+
+shortcuts = [
+ {  # Mapped to gesture 1 -> index finger!
+    'hotkey': ('Win', '1'),
+    'message': 'Google Chrome Launched'
+ },
+ {
+    # Mapped to gesture 2
+    'hotkey': ('Win', '2'),
+    'message': 'VS Code Launched'
+ },
+ {
+    # Mapped to gesture 3
+    'hotkey': ('Win', '3'),
+    'message': 'Terminal Launched'
+ },
+ {
+    # Mapped to gesture 4
+    'hotkey': ('Win', '4'),
+    'message': 'Filesystem Launched'
+ },
+ {
+    # Mapped to gesture Like!     
+    'hotkey': ('ctrl', 'alt', 't'),
+    'message': 'New Terminal'
+ },
+ {
+    # Mapped to gesture V! -> used for VS code!
+    'hotkey': ('Win', '2'),
+    'message': 'VS Code Launched'
+ },
+]
