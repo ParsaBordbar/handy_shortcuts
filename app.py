ee6c26840169884a2f79a92dc8fdf35e33846389
ParsaBordbar <parsab71@gmail.com>
2025-01-26 12:21:22 +0330

    feat: fist gesture using hand_olandmarks func

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import pyautogui
+import time
 from hand_landmarks import get_finger_data
 
 mp_hands = mp.solutions.hands
@@ -8,21 +9,54 @@ hands = mp_hands.Hands(max_num_hands=2)
 mp_drawing = mp.solutions.drawing_utils
 
 
-
 def is_peace_sign(hand_landmarks):
-    index_tip = hand_landmarks.landmark[8]
-    middle_tip = hand_landmarks.landmark[12]
-    ring_tip = hand_landmarks.landmark[16]
-    pinky_tip = hand_landmarks.landmark[20]
+    fingers = get_finger_data(hand_landmarks)
+
+    thumb_tip = fingers['thumb']['thumb_tip']
+    thumb_bottom = fingers['thumb']['thumb_mcp']
+
+    index_tip = fingers['index']['index_tip']
+    index_bottom = fingers['index']['index_mcp']
+
+    middle_tip = fingers['middle']['middle_tip']
+    middle_bottom = fingers['middle']['middle_mcp']
+
+    ring_tip = fingers['ring']['ring_tip']
+    ring_bottom = fingers['ring']['ring_mcp']
     
-    # Check if index and middle are up, others down
-    return (index_tip.y < hand_landmarks.landmark[5].y and
-            middle_tip.y < hand_landmarks.landmark[9].y and
-            ring_tip.y > hand_landmarks.landmark[13].y and
-            pinky_tip.y > hand_landmarks.landmark[17].y)
+    pinky_tip = fingers['pinky']['pinky_tip']
+    pinky_bottom = fingers['pinky']['pinky_mcp']
+
+    return (index_tip.y < index_bottom.y and
+            middle_tip.y < middle_bottom.y and
+            ring_tip.y > ring_bottom.y and
+            pinky_tip.y > pinky_bottom.y and 
+            thumb_tip.y < thumb_bottom.y )
+
 
 def is_fist_sign(hand_landmarks):
-    return (get_finger_data)
+    fingers = get_finger_data(hand_landmarks)
+
+    thumb_tip = fingers['thumb']['thumb_tip']
+    thumb_bottom = fingers['thumb']['thumb_mcp']
+
+    index_tip = fingers['index']['index_tip']
+    index_bottom = fingers['index']['index_mcp']
+
+    middle_tip = fingers['middle']['middle_tip']
+    middle_bottom = fingers['middle']['middle_mcp']
+
+    ring_tip = fingers['ring']['ring_tip']
+    ring_bottom = fingers['ring']['ring_mcp']
+    
+    pinky_tip = fingers['pinky']['pinky_tip']
+    pinky_bottom = fingers['pinky']['pinky_mcp']
+
+    return (index_tip.y < index_bottom.y and
+            middle_tip.y < middle_bottom.y and
+            ring_tip.y < ring_bottom.y and
+            pinky_tip.y < pinky_bottom.y and 
+            thumb_tip.y < thumb_bottom.y )
 
 # webcam
 video_cap = cv2.VideoCapture(0)
@@ -39,9 +73,13 @@ while video_cap.isOpened():
             
             # Detect gestures
             if is_peace_sign(hand_landmarks):
-                pyautogui.hotkey('ctrl', 'alt', 't')  # Open terminal
-                cv2.putText(frame, "TERMINAL OPENED", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                cv2.putText(frame, "Open Vs code", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                pyautogui.hotkey('win', '2')
                 break
+            if is_fist_sign(hand_landmarks):
+                cv2.putText(frame, "TERMINAL OPENED", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                pyautogui.hotkey('ctrl', 'alt', 't')
+                
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
@@ -53,6 +91,5 @@ while video_cap.isOpened():
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Cleanup
 video_cap.release()
 cv2.destroyAllWindows()
