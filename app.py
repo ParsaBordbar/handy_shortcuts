ac48bb49283cd5d617b446ee96000034245f4b64
ParsaBordbar <parsab71@gmail.com>
2025-01-26 14:13:51 +0330

    feat: new gestures for number 1-4 maped to taskbar

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import cv2
 import mediapipe as mp
 import pyautogui
 import time
-
-from gestures import is_fist, is_V
+from gestures import *
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=2)
@@ -33,13 +32,52 @@ while video_cap.isOpened():
     
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(frame_rgb)
-    
+
     action_performed = False
     
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
             
+            if handle_gesture_action(
+                is_one(hand_landmarks),
+                lambda: pyautogui.hotkey('Win', '1'),
+                cool_down=True
+            ):
+                cv2.putText(frame, "VS Code Launched", (50, 50), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                action_performed = True
+
+            if handle_gesture_action(
+                is_two(hand_landmarks),
+                lambda: pyautogui.hotkey('Win', '2'),
+                cool_down=True
+            ):
+                cv2.putText(frame, "VS Code Launched", (50, 50), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                action_performed = True
+
+            
+            if handle_gesture_action(
+                is_three(hand_landmarks),
+                lambda: pyautogui.hotkey('Win', '3'),
+                cool_down=True
+            ):
+                cv2.putText(frame, "VS Code Launched", (50, 50), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                action_performed = True
+
+
+            if handle_gesture_action(
+                is_four(hand_landmarks),
+                lambda: pyautogui.hotkey('Win', '4'),
+                cool_down=True
+            ):
+                cv2.putText(frame, "VS Code Launched", (50, 50), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                action_performed = True
+
+                
             if handle_gesture_action(
                 is_V(hand_landmarks),
                 lambda: pyautogui.hotkey('Win', '2'),
@@ -58,6 +96,7 @@ while video_cap.isOpened():
                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
                 action_performed = True
     
+
     cv2.imshow("Hand Detection", frame)    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
